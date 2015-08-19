f73cd50cfb27ef0f90f28ec161d0178fe3420640
Ryan <ryan@burgettweb.net>
2015-08-19 09:31:46 -0400

    Created tests file

---
+++ test/tests.js
@@ -0,0 +1,8 @@
+var pseries = require('../lib/pseries'),
+    should = require('should');
+
+describe(pseries, function() {
+    it('should exist', function() {
+        pseries.should.be.ok;
+    });
+});
