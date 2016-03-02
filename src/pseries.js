3d0e383518c448150fbd9452391bd368aacd81b1
Ryan <ryan@burgettweb.net>
2016-03-01 20:34:19 -0500

    Added array and function type checking

--- src/pseries.js
+++ src/pseries.js
@@ -1,18 +1,28 @@
 /**
- * pseries - a JavaScript micro-library for dealing with asynchronous flow control
+ * pseries - a JavaScript micro-library for handling asynchronous flow control
  * https://github.com/rBurgett/pseries
  *
- * Copyright (c) 2015 by Ryan Burgett.
+ * Copyright (c) 2016 by Ryan Burgett.
  * Licensed under Apache License Version 2.0
  * https://github.com/rBurgett/pseries/blob/master/LICENSE
- */
+ **/
 
 const pseries = arr => {
     return new Promise((resolve, reject) => {
 
+        if(!Array.isArray(arr) || arr.length === 0) {
+            reject(new Error('You must pass in an array of promise-returning functions.'));
+        }
+
         const runFuncs = (funcArr, i = 0, resArray = []) => {
 
-            funcArr[i]().then(
+            const func = funcArr[i];
+
+            if(typeof func !== 'function') {
+                reject(new Error('The passed-in array must only contain promise-returning functions.'));
+            }
+
+            func().then(
                 res => {
                     const newResArray = resArray.concat([res]);
                     if(i === funcArr.length - 1) {
@@ -23,6 +33,7 @@ const pseries = arr => {
                 },
                 err => reject(err)
             )
+
         }
 
         runFuncs(arr);
